9012e648222861bc9443a0b951a9c5a6f61654b9
Albin Antony <albinantony2420@gmail.com>
2026-02-14 21:27:02 +0530

    template

---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:calendar/src/presentation/screens/splash_screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get.dart';
+
+class Calendar extends StatelessWidget {
+  const Calendar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(builder: EasyLoading.init(), home: SplashScreen());
+  }
+}
